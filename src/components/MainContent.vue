0d0fbe7f5ff7bdf194f8205dd46af04d7bdeaab0
Mathilde Wattrelos <mat.wattrelos@yahoo.com>
2021-05-10 14:43:12 +0100

    Add image background

--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="relative bg-gray-600 md:w-4/5 h-full">
+  <div class="relative bg-gray-600 md:w-4/5 h-full bg-center bg-no-repeat bg-cover" style="background-image: url('src/assets/day.jpg')">
     <Now v-if="info" :current="info.current"/>
   </div>
 </template>
--- src/components/Now.vue
+++ src/components/Now.vue
@@ -2,7 +2,7 @@
   <div class="absolute inset-x-0 bottom-0">
     <div class="flex items-center p-10">
       <h2 class="text-9xl mr-28 font-semibold">{{ current.temp }}°C</h2>
-      <div class="">
+      <div class="flex flex-col h-32 justify-around">
         <h3 class="text-5xl">Today is a rainy day</h3>
         <p class="text-2xl">Don't forget an umbrella</p>
       </div>
