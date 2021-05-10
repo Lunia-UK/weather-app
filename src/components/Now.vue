42766b51f21642283d469e141b1c948d0fd10b4b
Mathilde Wattrelos <mat.wattrelos@yahoo.com>
2021-05-10 13:15:25 +0100

    Add informations

--- src/components/Now.vue
+++ src/components/Now.vue
@@ -1,6 +1,17 @@
 <template>
-  <div class="absolute inset-x-0 bottom-0 h-16">
-    <h1>{{ current.temp }}</h1>
+  <div class="absolute inset-x-0 bottom-0">
+    <div class="flex items-center p-10">
+      <h2 class="text-9xl mr-28 font-semibold">{{ current.temp }}°C</h2>
+      <div class="">
+        <h3 class="text-5xl">Today is a rainy day</h3>
+        <p class="text-2xl">Don't forget an umbrella</p>
+      </div>
+    </div>
+    <hr>
+    <div class="flex justify-between p-5">
+      <p>{{time}}</p>
+      <p class="uppercase">maidstone, gb</p>
+    </div>
   </div>
 </template>
 
