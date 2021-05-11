8adefbedc6836baa7b0d96a3e9412e31d17030c7
Mathilde Wattrelos <mat.wattrelos@yahoo.com>
2021-05-11 15:05:45 +0100

    Change background image according to the weather

--- src/components/Now.vue
+++ src/components/Now.vue
@@ -2,8 +2,8 @@
   <div class="absolute inset-x-0 bottom-0">
     <div class="flex items-center p-10">
       <h2 class="text-9xl mr-28 font-semibold">{{ current.temp }}°C</h2>
-      <div class="flex flex-col h-32 justify-around">
-        <h3 class="text-5xl">Today is a rainy day</h3>
+      <div class="flex flex-col h-32 justify-around ">
+        <h3 class="text-5xl font-semibold">Today is a rainy day</h3>
         <p class="text-2xl">Don't forget an umbrella</p>
       </div>
     </div>
